af9c165dc170bc374cb9e810a0a9ac4e8071c136
haibane_tenshi <obscure.sanctum@gmail.com>
2022-07-12 00:18:23 +0300

    Relax some `Handle`'s ops implementations to allow unsized T.

--- src/context/handle.rs
+++ src/context/handle.rs
@@ -58,88 +58,145 @@ where
     }
 }
 
-impl<'s, 'a, T> Reborrow2Lifetime<'s> for HandleRef<'a, T> {
+impl<'s, 'a, T> Reborrow2Lifetime<'s> for HandleRef<'a, T>
+where
+    T: ?Sized + 'a,
+{
     type Output = Self;
 }
 
-impl<'a, T> Reborrow2 for HandleRef<'a, T> {
+impl<'a, T> Reborrow2 for HandleRef<'a, T>
+where
+    T: ?Sized + 'a,
+{
     fn reborrow(&mut self) -> <Self as Reborrow2Lifetime<'_>>::Output {
         *self
     }
 }
 
-impl<'s, 'a, T> Reborrow2Lifetime<'s> for HandleMut<'a, T> {
+impl<'s, 'a, T> Reborrow2Lifetime<'s> for HandleMut<'a, T>
+where
+    T: ?Sized + 'a,
+{
     type Output = HandleMut<'s, T>;
 }
 
-impl<'a, T> Reborrow2 for HandleMut<'a, T> {
+impl<'a, T> Reborrow2 for HandleMut<'a, T>
+where
+    T: ?Sized + 'a,
+{
     fn reborrow(&mut self) -> <Self as Reborrow2Lifetime<'_>>::Output {
         HandleMut::new(&mut *self.0)
     }
 }
 
-impl<'a, T> Coerce<HandleRef<'a, T>> for HandleRef<'a, T> {
+impl<'a, T> Coerce<HandleRef<'a, T>> for HandleRef<'a, T>
+where
+    T: ?Sized + 'a,
+{
     fn coerce(self) -> HandleRef<'a, T> {
         self
     }
 }
 
-impl<'a, T> Coerce<HandleRef<'a, T>> for HandleMut<'a, T> {
+impl<'a, T> Coerce<HandleRef<'a, T>> for HandleMut<'a, T>
+where
+    T: ?Sized + 'a,
+{
     fn coerce(self) -> HandleRef<'a, T> {
         Handle(self.0)
     }
 }
 
-impl<'a, T> Coerce<HandleMut<'a, T>> for HandleMut<'a, T> {
+impl<'a, T> Coerce<HandleMut<'a, T>> for HandleMut<'a, T>
+where
+    T: ?Sized + 'a,
+{
     fn coerce(self) -> HandleMut<'a, T> {
         self
     }
 }
 
-impl<'a, T> UnifyOp<HandleRef<'a, T>> for HandleRef<'a, T> {
+impl<'a, T> UnifyOp<HandleRef<'a, T>> for HandleRef<'a, T>
+where
+    T: ?Sized + 'a,
+{
     type Output = HandleRef<'a, T>;
 }
 
-impl<'a, T> UnifyOp<HandleMut<'a, T>> for HandleRef<'a, T> {
+impl<'a, T> UnifyOp<HandleMut<'a, T>> for HandleRef<'a, T>
+where
+    T: ?Sized + 'a,
+{
     type Output = HandleMut<'a, T>;
 }
 
-impl<'a, T> UnifyOp<HandleRef<'a, T>> for HandleMut<'a, T> {
+impl<'a, T> UnifyOp<HandleRef<'a, T>> for HandleMut<'a, T>
+where
+    T: ?Sized + 'a,
+{
     type Output = HandleMut<'a, T>;
 }
 
-impl<'a, T> UnifyOp<HandleMut<'a, T>> for HandleMut<'a, T> {
+impl<'a, T> UnifyOp<HandleMut<'a, T>> for HandleMut<'a, T>
+where
+    T: ?Sized + 'a,
+{
     type Output = HandleMut<'a, T>;
 }
 
-impl<'a, T> AsPermissionOp for HandleRef<'a, T> {
+impl<'a, T> AsPermissionOp for HandleRef<'a, T>
+where
+    T: ?Sized + 'a,
+{
     type Output = Shared;
 }
 
-impl<'a, T> AsPermissionOp for HandleMut<'a, T> {
+impl<'a, T> AsPermissionOp for HandleMut<'a, T>
+where
+    T: ?Sized + 'a,
+{
     type Output = Mutable;
 }
 
-impl<'a, T> LimitByOp<()> for HandleRef<'a, T> {
+impl<'a, T> LimitByOp<()> for HandleRef<'a, T>
+where
+    T: ?Sized + 'a,
+{
     type Output = option::None;
 }
 
-impl<'a, T> LimitByOp<Shared> for HandleRef<'a, T> {
+impl<'a, T> LimitByOp<Shared> for HandleRef<'a, T>
+where
+    T: ?Sized + 'a,
+{
     type Output = option::Some<Self>;
 }
 
-impl<'a, T> LimitByOp<Mutable> for HandleRef<'a, T> {
+impl<'a, T> LimitByOp<Mutable> for HandleRef<'a, T>
+where
+    T: ?Sized + 'a,
+{
     type Output = option::Some<Self>;
 }
 
-impl<'a, T> LimitByOp<()> for HandleMut<'a, T> {
+impl<'a, T> LimitByOp<()> for HandleMut<'a, T>
+where
+    T: ?Sized + 'a,
+{
     type Output = option::None;
 }
 
-impl<'a, T> LimitByOp<Shared> for HandleMut<'a, T> {
+impl<'a, T> LimitByOp<Shared> for HandleMut<'a, T>
+where
+    T: ?Sized + 'a,
+{
     type Output = option::Some<HandleRef<'a, T>>;
 }
 
-impl<'a, T> LimitByOp<Mutable> for HandleMut<'a, T> {
+impl<'a, T> LimitByOp<Mutable> for HandleMut<'a, T>
+where
+    T: ?Sized + 'a,
+{
     type Output = option::Some<Self>;
 }
